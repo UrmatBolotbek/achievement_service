c88941b286d8ab51ef8ba654b0a67f167e4bcc4e
Roman Nifontov <romannif@mail.ru>
2024-12-08 23:21:36 +0500

    промежуточный коммит

--- src/main/java/faang/school/achievement/event_handler/HandsomeAchievementHandler.java
+++ src/main/java/faang/school/achievement/event_handler/HandsomeAchievementHandler.java
@@ -1,9 +1,23 @@
 package faang.school.achievement.event_handler;
 
+import faang.school.achievement.event.ProfilePicEvent;
+import faang.school.achievement.service.AchievementService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public abstract class HandsomeAchievementHandler implements EventHandler {
+@RequiredArgsConstructor
+public abstract class HandsomeAchievementHandler implements EventHandler<ProfilePicEvent> {
+
+    @Value("${achievement.profile-pic}")
+    private String profilePic;
+
+    private final AchievementService achievementService;
+
+
+
+
 
 
 }
--- src/main/java/faang/school/achievement/service/AchievementService.java
+++ src/main/java/faang/school/achievement/service/AchievementService.java
@@ -1,11 +1,21 @@
 package faang.school.achievement.service;
 
+import faang.school.achievement.repository.UserAchievementRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class AchievementService {
+
+    private final UserAchievementRepository userAchievementRepository;
+
+
+    public boolean hasAchievement(long userId, long achievementId) {
+        return userAchievementRepository.existsByUserIdAndAchievementId(userId, achievementId);
+    }
 
 
 
