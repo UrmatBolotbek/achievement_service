b5390ca5fa49a8a91bb313a2b0add6cf33a9c158
Roma Nifontov <romannif@mail.ru>
2024-12-10 17:15:54 +0500

    добавил значения в yaml

--- src/main/java/faang/school/achievement/config/async/AsyncConfiguration.java
+++ src/main/java/faang/school/achievement/config/async/AsyncConfiguration.java
@@ -1,5 +1,6 @@
 package faang.school.achievement.config.async;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,10 +11,16 @@ import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class AsyncConfiguration {
+    @Value("${async.pool-size}")
+    private int poolSize;
+    @Value("${async.max-pool-size}")
+    private int maxPoolSize;
+    @Value("${async.keep-alive-time}")
+    private long keepAliveTime;
 
     @Bean(name = "fixedThreadPool")
     public ExecutorService fixedThreadPoolTaskExecutor() {
-        return new ThreadPoolExecutor(10,20,10,
+        return new ThreadPoolExecutor(poolSize, maxPoolSize, keepAliveTime,
                 TimeUnit.SECONDS, new LinkedBlockingQueue<>());
     }
 }
