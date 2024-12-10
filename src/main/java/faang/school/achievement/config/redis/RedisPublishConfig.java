def7c1748c90b337d9ccfc70c667b14d6d727f44
Roman Nifontov <romannif@mail.ru>
2024-12-10 19:26:38 +0500

    промежуточный коммит

---
+++ src/main/java/faang/school/achievement/config/redis/RedisPublishConfig.java
@@ -0,0 +1,33 @@
+package faang.school.achievement.config.redis;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisPublishConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    @Bean
+    public JedisConnectionFactory redisPublishConnectionFactory() {
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration(redisHost, redisPort);
+        return new JedisConnectionFactory(redisConfig);
+    }
+
+    @Bean("redisPublishTemplate")
+    public RedisTemplate<String, Object> redisPublishTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisPublishConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        return template;
+    }
+}
--- src/main/java/faang/school/achievement/publisher/EventPublisher.java
+++ src/main/java/faang/school/achievement/publisher/EventPublisher.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import faang.school.achievement.event.PublishEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
@@ -16,14 +17,14 @@ public class EventPublisher {
 
     @Value("${spring.data.redis.channels.achievement-channel}")
     private String topicAchievement;
-
-    private final RedisTemplate<String, Object> redisTemplate;
+    @Qualifier("redisPublishTemplate")
+    private final RedisTemplate<String, Object> redisPublishTemplate;
     private final ObjectMapper objectMapper;
 
     public void publish(PublishEvent event) {
         try {
             String eventToPublish = objectMapper.writeValueAsString(event);
-            redisTemplate.convertAndSend(topicAchievement, eventToPublish);
+            redisPublishTemplate.convertAndSend(topicAchievement, eventToPublish);
         } catch (JsonProcessingException e) {
             log.error("An error occurred while working with JSON: ", e);
             throw new RuntimeException(e);
