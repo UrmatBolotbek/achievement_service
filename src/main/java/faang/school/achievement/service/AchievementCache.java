edcd99969dbb419e3a8f62424a21f56d7fcc0169
UrmatBolotbek <urmar9797@gmail.com>
2024-12-11 18:06:07 +0100

    добавил логи

--- src/main/java/faang/school/achievement/service/AchievementCache.java
+++ src/main/java/faang/school/achievement/service/AchievementCache.java
@@ -4,19 +4,21 @@ import faang.school.achievement.model.dto.AchievementDto;
 import faang.school.achievement.model.mapper.AchievementMapper;
 import faang.school.achievement.repository.AchievementRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AchievementCache {
     private static final String KEY_FORMAT = "%s:%s";
-    private static final String PREFIX = "achievement:";
+    private static final String PREFIX = "achievement";
 
     private final AchievementRepository achievementRepository;
     private final RedisTemplate<String, AchievementDto> achievementRedisTemplate;
@@ -25,12 +27,18 @@ public class AchievementCache {
     @EventListener(ApplicationReadyEvent.class)
     @Async("fixedThreadPool")
     public void initCache() {
+        log.info("Starting cache initialization");
         List<AchievementDto> allAchievementDtos = achievementRepository.findAll().stream()
                 .map(achievementMapper::toDto)
                 .toList();
 
-        for (AchievementDto achievementDto : allAchievementDtos) {
-            achievementRedisTemplate.opsForValue().set(buildKey(achievementDto.getTitle()), achievementDto);
+        if (allAchievementDtos.isEmpty()) {
+            log.info("No achievements found in database. Cache is empty.");
+        } else {
+            for (AchievementDto achievementDto : allAchievementDtos) {
+                achievementRedisTemplate.opsForValue().set(buildKey(achievementDto.getTitle()), achievementDto);
+            }
+            log.info("Cache initialization completed. Loaded {} achievements.", allAchievementDtos.size());
         }
     }
 
