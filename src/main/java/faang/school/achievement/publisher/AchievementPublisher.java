4bb25b3ed3b13753eb4821e6f7ec44176d41df4c
zlomilk95 <kenobi_95@mail.ru>
2024-12-15 17:30:26 +0300

    BJS2-41616 edits

--- src/main/java/faang/school/achievement/publisher/AchievementPublisher.java
+++ src/main/java/faang/school/achievement/publisher/AchievementPublisher.java
@@ -1,20 +1,33 @@
 package faang.school.achievement.publisher;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import faang.school.achievement.event.PublishEvent;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class AchievementPublisher {
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final ObjectMapper objectMapper;
+
     @Value("${spring.data.redis.channels.achievement-channel}")
     private String topicAchievement;
-    private final RedisTemplate<String, Object> redisTemplate;
 
     public void publish(PublishEvent event) {
-        redisTemplate.convertAndSend(topicAchievement, event);
+        try {
+            String eventToPublish = objectMapper.writeValueAsString(event);
+            redisTemplate.convertAndSend(topicAchievement, eventToPublish);
+        } catch (JsonProcessingException e) {
+            log.error("An error occurred while working with JSON: ", e);
+            throw new RuntimeException(e);
+        }
     }
 }
