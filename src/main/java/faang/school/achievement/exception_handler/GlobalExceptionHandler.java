74d33e287e3cd7e56af8d49c8f17e82bf0306353
Roman Nifontov <romannif@mail.ru>
2024-12-25 13:09:11 +0500

    проверяю Ci

--- src/main/java/faang/school/achievement/exception_handler/GlobalExceptionHandler.java
+++ src/main/java/faang/school/achievement/exception_handler/GlobalExceptionHandler.java
@@ -1,20 +1,23 @@
 package faang.school.achievement.exception_handler;
 
-import faang.school.achievement.exception.ErrorResponse;
 import jakarta.persistence.EntityNotFoundException;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponse handleEntityNotFoundException(EntityNotFoundException e) {
-        return new ErrorResponse(e.getMessage());
+    public ResponseEntity<ProblemDetail> handleEntityNotFoundException(EntityNotFoundException e) {
+        return buildProblemDetailResponse(HttpStatus.NOT_FOUND, e.getMessage());
+    }
+
+    private ResponseEntity<ProblemDetail> buildProblemDetailResponse(HttpStatus status, String detail) {
+        return ResponseEntity.status(status).body(ProblemDetail.forStatusAndDetail(status, detail));
     }
 
 }
