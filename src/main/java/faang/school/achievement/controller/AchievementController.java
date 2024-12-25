38ff4b9849e12333a50a4afddf76905700f21a5d
Roman Nifontov <romannif@mail.ru>
2024-12-25 22:23:45 +0500

    проверяю ci

--- src/main/java/faang/school/achievement/controller/AchievementController.java
+++ src/main/java/faang/school/achievement/controller/AchievementController.java
@@ -21,6 +21,11 @@ public class AchievementController {
     private final AchievementService achievementService;
     private final UserContext userContext;
 
+    @GetMapping("/user/{userId}")
+    public List<AchievementResponseDto> getAchievementsWithUserId(@PathVariable long userId) {
+        return achievementService.getAchievementsByUserId(userId);
+    }
+
     @GetMapping("/filters")
     public List<AchievementResponseDto> getAchievements(@ModelAttribute AchievementRequestFilterDto requestDto) {
         return achievementService.getAchievementsWithFilters(requestDto);
