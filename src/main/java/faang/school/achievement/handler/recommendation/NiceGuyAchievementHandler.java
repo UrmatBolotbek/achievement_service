143c0a499ec50137aea242dcb0d99c993c661054
Alena Shmelkova <AlenaShmelkova@gmail.com>
2025-01-11 11:13:01 -0500

    BJS2-41625 злобный комментатор убрала аннотацию  transactional

--- src/main/java/faang/school/achievement/handler/recommendation/NiceGuyAchievementHandler.java
+++ src/main/java/faang/school/achievement/handler/recommendation/NiceGuyAchievementHandler.java
@@ -4,7 +4,6 @@ import faang.school.achievement.event.RecommendationEvent;
 import faang.school.achievement.handler.AbstractAchievementHandler;
 import faang.school.achievement.model.AchievementTitle;
 import faang.school.achievement.service.AchievementService;
-import jakarta.transaction.Transactional;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
@@ -17,7 +16,6 @@ public class NiceGuyAchievementHandler extends AbstractAchievementHandler<Recomm
 
     @Override
     @Async("fixedThreadPool")
-    @Transactional
     public void handle(RecommendationEvent event) {
         handleAchievement(event.getAuthorId(), AchievementTitle.NICE_GUY);
     }
